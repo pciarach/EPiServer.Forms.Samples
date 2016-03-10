90fd702d8b4d04f640eaf18fd4ebb0803990b286
Thach Lockevn <lockevn@gmail.com>
2016-03-10 15:07:21 +0700

    Update Samples package 1.0.2.9000

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -15,5 +15,5 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.1.9000")]
-[assembly: AssemblyFileVersion("1.0.1.9000")]
+[assembly: AssemblyVersion("1.0.2.9000")]
+[assembly: AssemblyFileVersion("1.0.2.9000")]
